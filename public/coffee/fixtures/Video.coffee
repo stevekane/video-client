cf2ff47077bfa22d8d50ac19d2675ced6c7c51c6
steve kane <skane@4-south.com>
2013-11-12 23:41:29 -0600

    added subtitle t fixtures and to deail view

--- public/coffee/fixtures/Video.coffee
+++ public/coffee/fixtures/Video.coffee
@@ -4,16 +4,20 @@ video1 =
     #Hey you guys\n###let's talk about why this isn't workin\nSometimes it just seems like the devil is going to take all the cheese na mean?  Ya dawg, dat cheeze.
     """,
   title: "Whatever you want bro"
+  subtitle: "Johnny 5 checks out for good"
 
 video2 =
   id: 1,
   body: "###Heyoooo",
   title: "Freedom from tyranny"
+  subtitle: "How the west was won"
 
 video3 =
   id: 2,
   body: "#Yo dog",
   title: "Do it up",
+  subtitle: "Darmok and Jalad at Tanagra"
+
 App.Video.FIXTURES = [
   video1, video2, video3
 ]
--- public/dist/app.js
+++ public/dist/app.js
@@ -59,19 +59,22 @@ var video1, video2, video3;
 video1 = {
   id: 0,
   body: "#Hey you guys\n###let's talk about why this isn't workin\nSometimes it just seems like the devil is going to take all the cheese na mean?  Ya dawg, dat cheeze.",
-  title: "Whatever you want bro"
+  title: "Whatever you want bro",
+  subtitle: "Johnny 5 checks out for good"
 };
 
 video2 = {
   id: 1,
   body: "###Heyoooo",
-  title: "Freedom from tyranny"
+  title: "Freedom from tyranny",
+  subtitle: "How the west was won"
 };
 
 video3 = {
   id: 2,
   body: "#Yo dog",
-  title: "Do it up"
+  title: "Do it up",
+  subtitle: "Darmok and Jalad at Tanagra"
 };
 
 App.Video.FIXTURES = [video1, video2, video3];
